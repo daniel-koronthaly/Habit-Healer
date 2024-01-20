0291bb72c3dc8c91a68127561f71d17312192d69
Daniel Koronthály <daniel@koronthaly.net>
2024-01-19 16:04:30 -0800

    added new pages

---
+++ habit-healer/components/Calendar.jsx
@@ -0,0 +1,12 @@
+import {
+    Text,
+    View,
+} from 'react-native'
+
+const Calendar = () => (
+    <View>
+        <Text>Calendar</Text>
+    </View>
+)
+
+export default Calendar
---
+++ habit-healer/components/Habit.jsx
@@ -0,0 +1,12 @@
+import {
+    Text,
+    View,
+} from 'react-native'
+
+const Habit = () => (
+    <View>
+        <Text>Habit</Text>
+    </View>
+)
+
+export default Habit
